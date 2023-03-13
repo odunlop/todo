74e168986e1c19fa9ecfef35ca2700fe5371fdf3
Orla Dunlop <orla.dunlop@engineering.digital.dwp.gov.uk>
2023-03-13 14:04:45 +0000

    feat(*): Add label functionality

---
+++ tests/test_trello.py
@@ -0,0 +1,3 @@
+# Create test_board
+
+# Delete test_board at the end
--- trello.py
+++ trello.py
@@ -1,15 +1,22 @@
+from cProfile import label
 from configparser import MissingSectionHeaderError
 import os
 import requests
 import json
 import get_todo
+import random
+import string
 
 api_key = os.environ.get('TRELLO_API')
 token = os.environ.get('TRELLO_TOKEN')
 board_id_hardcoded = "6409b884e0650c7206272710"
-url = "https://trello.com/b/KZItRtcg/api-testing"
+board_url = "https://trello.com/b/KZItRtcg/api-testing"
+# repo_name = "Trello Testing Project" # Get this from git???
+repo_name = ''.join(random.choices(string.ascii_uppercase +string.digits, k=6))
 
 # curl 'https://trello.com/b/KZItRtcg/api-testing.json?key=${TRELLO_API}&token=${TRELLO_TOKEN}'
+
+### BOARDS ###
 
 def get_board_id(url):
     headers = {
@@ -51,6 +58,116 @@ def get_lists(board_id):
     lists = json.loads(response.text)
     return lists
 
+### LABELS ###
+
+def get_all_labels(board_id):
+    url = f"https://api.trello.com/1/boards/{board_id}/labels"
+
+    query = {
+        'key': api_key,
+        'token': token
+    }
+
+    response = requests.request(
+        "GET",
+        url,
+        params=query
+    )
+
+    return json.loads(response.text)
+
+def get_free_labels(labels):
+    free_labels = []
+    for label in labels:
+        if label["name"] == "":
+            free_labels.append(label["id"])
+    return free_labels
+
+def give_avaliable_colour(labels):
+    # have an array with all colours, if already one with name filled in, skip
+    label_colours = ['green', 'yellow', 'orange', 'red', 'purple', 'blue', 'sky', 'lime', 'pink', 'black', "green_light", "green_dark", "yellow_light", "yellow_dark", "orange_light", "orange_dark", "red_light", "red_dark", "purple_light", "purple_dark", "blue_light", "blue_dark", "sky_light", "sky_dark", "lime_light", "lime_light", "pink_light", "pink_dark", "black_light", "black_dark"]
+    
+    colours_in_use = []
+
+    for label in labels:
+        if label["name"] != "":
+            colours_in_use.append(label["color"])
+    
+    for colour in colours_in_use:
+        label_colours.remove(colour)
+    
+    print(label_colours)
+
+    return label_colours[0]
+
+def amend_label(label_id, repo_name):
+    url = f"https://api.trello.com/1/labels/{label_id}"
+
+    query = {
+        'key': api_key,
+        'token': token,
+        'name': repo_name
+    }
+
+    response = requests.request(
+        "PUT",
+        url,
+        params=query
+    )
+
+    return json.loads(response.text)
+
+def new_label(board_id, repo_name, colour):
+    colour = colour
+    url = f"https://api.trello.com/1/boards/{board_id}/labels"
+
+    query = {
+        "name": repo_name,
+        "color": colour,
+        "key": api_key,
+        "token": token
+    }
+
+    response = requests.request(
+        "POST",
+        url,
+        params=query
+    )
+
+    return json.loads(response.text)
+
+def label_project(board_id, repo_name, labels):
+    # labels = get_all_labels(board_id)
+
+    # Check if any avaliable labels are free
+    free_labels = get_free_labels(labels)
+    print(free_labels)
+    if free_labels != []:
+        label = amend_label(free_labels[0], repo_name)
+    else:
+        colour = give_avaliable_colour(labels)
+        label = new_label(board_id, repo_name, colour)
+    
+    print("Label for project created")
+    return label["id"]
+
+
+def get_project_label(board_id, repo_name):
+    labels = get_all_labels(board_id)
+    exists = False
+    # check if there's a label, if not run label_project
+    # we get this by looking through all labels for same name
+    for label in labels:
+        if label["name"] == repo_name:
+            exists = True
+            return label["id"]
+    
+    if exists == False:
+        label_id = label_project(board_id, repo_name, labels)
+        return label_id
+
+### CARDS ###
+
 def get_todo_list_id(lists):
     for list in lists:
         if list["name"] == "TO-DO":
@@ -83,7 +200,6 @@ def new_card(list_id, name, desc):
     json_response = json.loads(response.text)
     return json_response["id"], json_response["idShort"]
 
-# Now we want to get the todos back with their corresponding ids....
 # TO_DO: Response if no new tickets
 def write_todos(dir, list_id):
     todo_tickets = []
@@ -107,7 +223,6 @@ def write_todos(dir, list_id):
             print(".", end="", flush=True)        
     return todo_tickets
 
-
 # And then add the id to the line....
 def remember_todo(tickets):
 # 1. Loop through each ticket
@@ -122,18 +237,27 @@ def remember_todo(tickets):
             file.write(data)
 
 
-# Let's get it to only add tickets if not already existing
+# THINGS TO DO:
+# Checks if placed in Done??? Maybe allow users to configure the name of their "done" column 
 
+# Checks if existing cards still exist in thingy
 
-# Then when it goes to check, it finds a card with the same shortid and checks the long ids match (long ids stored in a .todo file??? maybe use the python to yml thing)
-# ALSO need to make sure it's not in "in progress" or any other list in the board
+# Colour categorise?? Also add from which repo to card notes
 
-board_id = get_board_id(url)
-lists = get_lists(board_id)
-list_id = get_todo_list_id(lists)
-dir = "."
-tickets = write_todos(dir, list_id)
-remember_todo(tickets)
+# Response if no new tickets
+
+# Set ENV for colour categorise that users can set as true or false
+
+board_id = get_board_id(board_url)
+# label_project(board_id, repo_name)
+label = get_project_label(board_id, repo_name)
+print(label)
+
+# lists = get_lists(board_id)
+# list_id = get_todo_list_id(lists)
+# dir = "."
+# tickets = write_todos(dir, list_id)
+# remember_todo(tickets)
 
 # for ticket in tickets:
 #     print(ticket)
